3ab9ffb4b2fe2747ee9ad27ea7496ea4f6287f34
Rafael Panegassi <70807807+rafaelpanegassi@users.noreply.github.com>
2024-03-22 16:55:46 -0300

    update scripts220324

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+from pipeline_02 import pipeline
 
-def main():
-    st.write("Projeto Inicial")
+st.title('Processador de Arquivos')
 
-if __name__ == "__main__":
-    main()
+if st.button('Processar'):
+    with st.spinner('Processando...'):
+        logs = pipeline()
+        # Exibe os logs no Streamlit
+        for log in logs:
+            st.write(log)
